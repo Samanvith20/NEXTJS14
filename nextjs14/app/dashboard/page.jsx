6959fc37b9e605916e803e14c5c2a94a6b1405c0
Samanvith <Samanvith2005@gmail.com>
2024-03-21 19:12:33 +0530

    Added tranascationPage

--- nextjs14/app/dashboard/page.jsx
+++ nextjs14/app/dashboard/page.jsx
@@ -1,10 +1,13 @@
-
+import Transactions from "../ui/transcation/transcation";
+import styles from "../ui/dashboard/dashboard.module.css"
 
 const DashboardPage = () => {
     return (
-        <div>
+        <div className={styles.wrapper}>
+        <div className={styles.main}>
             
-            <h1>Dashboard Page</h1>
+            <Transactions/>
+        </div>
         </div>
     );
 };
